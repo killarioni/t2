af943e9a5b386c0d84b780f31ed8c35149949186
killarioni <128322372+killarioni@users.noreply.github.com>
2024-05-14 23:11:12 +0400

    Add files via upload

---
+++ react-blog/src/App.js
@@ -0,0 +1,16 @@
+import  NavBar from "./components/navbar/NavBar";
+import Home from "./pages/home/Home";
+import Single from "./pages/single/single";
+
+
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    <Single />
+    </>
+  );
+}
+
+export default App;
